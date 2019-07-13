ae054d5d9eb295e455b61dbbf4db90a0b33ac8db
rocinante02 <idervalsama@gmail.com>
2019-07-12 22:21:16 -0300

    DrawLine e DrawTriangle, porém sem interpolação

--- main.cpp
+++ main.cpp
@@ -6,15 +6,27 @@ void MyGlDraw(void)
 	//*************************************************************************
 	// Chame aqui as funções do mygl.h
 	//*************************************************************************
-	srand(time(NULL));
-	std::vector<Pixel> pixels;      //Vetor de pixels
-	for(int i = 0; i < 500; i++){
-	Pixel p = {i, i, rand() % 255, rand() % 255 , rand() % 255, 0}; //Cada pixel tem cores aleatorias
-	pixels.push_back(p);
-	}
-	for(int i; i < pixels.size(); i++){                             //desenha uma linha com cores aleatórias
-	putPixel(pixels[i]);
-	}
+    Pixel pxl1 = {100, 300, 255, 0, 0, 0};
+    Pixel pxl2 = {200, 150, 0, 0, 255, 0};
+
+    Pixel pxl3 = {400, 300, 0, 255, 0, 0};
+    Pixel pxl4 = {300, 150, 0, 0, 255, 0};
+
+    Pixel pxl5 = {150, 450, 255, 0, 0, 0};
+    Pixel pxl6 = {350, 450, 0, 255, 0, 0};
+
+    Pixel pxl7 = {100, 300, 255, 0, 0, 1};
+    Pixel pxl8 = {300, 400, 0, 255, 0, 1};
+    Pixel pxl9 = {400, 350, 0, 0, 255, 1};
+
+
+
+
+    drawLine(pxl1,pxl2);
+    drawLine(pxl3,pxl4);
+    drawLine(pxl5,pxl6);
+
+    drawTriangle(pxl7,pxl8,pxl9);
 
 }
 
